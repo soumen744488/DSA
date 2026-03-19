cf17f7cbe56a6178458184153de0cd9f97d87f15
Soumen <palsoumen306@gmail.com>
2026-03-19 17:07:24 +0530

    Get

---
+++ src/main/java/get/Main.java
@@ -0,0 +1,24 @@
+package get;
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+
+
+        System.out.println(myLinkedList.get(3).value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            3
+
+        */
+
+    }
+
+}
